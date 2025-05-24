ce511cb6aa8469c53e797fc1ff05e445addfce59
TheDoeMan <86815226+TheDoeMan@users.noreply.github.com>
2025-05-23 23:13:00 -0700

    Update app-ssl.js

--- app-ssl.js
+++ app-ssl.js
@@ -1,29 +1,19 @@
-const express = require('express')
+const express = require('express');
 const http = require('http');
-const https = require('https');
 const WebSocket = require('ws');
-const fs = require('fs');
 const Game = require('./game');
 
 const app = express();
-
-// SSL Files
-const sslOptions = {
-  cert: fs.readFileSync('./fullchain.pem'),
-  key: fs.readFileSync('./privkey.pem')
-};
-
 const server = http.createServer(app);
-const secureServer = https.createServer(sslOptions, app)
-const wss = new WebSocket.Server({ server: secureServer });
+const wss = new WebSocket.Server({ server });
 const rummy = new Game(wss);
 
-// Serve Static Files/Assets
+// Serve static files
 app.use(express.static('public'));
 
-// Ignore Socket Errors
-wss.on('error', () => console.log('*errored*'));
-wss.on('close', () => console.log('*disconnected*'));
+// Ignore WebSocket errors
+wss.on('error', () => console.log('* WebSocket error *'));
+wss.on('close', () => console.log('* WebSocket closed *'));
 
 /*----------------------ENDPOINTS----------------------*/
 app.get('/', (req, res) => {
@@ -31,27 +21,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/join/:lobby', (req, res) => {
-  let code = req.params.lobby;
+  const code = req.params.lobby;
   if (rummy.addLobby(code)) {
-    res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbys[code].token);
+    res.redirect('/game/' + code + '/' + rummy.lobbys[code].token);
   } else {
     res.redirect('/');
   }
 });
 
 app.get('/joincpu/:lobby', (req, res) => {
-  let code = req.params.lobby;
-  if (rummy.addLobby(code, cpu=true)) {
-    res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbys[code].token);
+  const code = req.params.lobby;
+  if (rummy.addLobby(code, true)) {
+    res.redirect('/game/' + code + '/' + rummy.lobbys[code].token);
   } else {
     res.redirect('/');
   }
 });
 
 app.get('/game/:lobby/:token', (req, res) => {
-  let code = "" + req.params.lobby,
-      token = req.params.token;
-  if (req.params.token && rummy.lobbys[code] && rummy.lobbys[code].token == token) {
+  const code = req.params.lobby;
+  const token = req.params.token;
+  if (rummy.lobbys[code] && rummy.lobbys[code].token === token) {
     res.sendFile(__dirname + '/public/game.html');
   } else {
     res.redirect('/');
@@ -59,10 +49,8 @@ app.get('/game/:lobby/:token', (req, res) => {
 });
 /*-----------------------------------------------------*/
 
-// Start Server
-server.listen(5000, () => {
-  console.log('Listening on port 5000...')
-});
-secureServer.listen(443, () => {
-  console.log('(Secure) Listening on port 443...')
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}...`);
 });
